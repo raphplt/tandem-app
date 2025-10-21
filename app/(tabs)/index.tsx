48838732a62c06b6b0f57112712dd4f754e21c7b
raphaeltkorp <raphael@tkorp.com>
2025-10-21 23:26:37 +0200

    feat: enhance project setup with new configurations and components
    - Added .env.example for environment variable management.
    - Updated .gitignore to exclude .env files.
    - Introduced .npmrc to manage npm configurations.
    - Enhanced babel.config.js with module resolver and plugins for better path management.
    - Created global.css for Tailwind CSS directives.
    - Updated package.json and package-lock.json with new dependencies including @gluestack-ui and related packages.
    - Refactored tailwind.config.js to support dark mode and extended theme configurations.
    - Implemented GluestackUIProvider for consistent theming across the application.
    - Added Box component for reusable layout styling.
    - Updated app layout and HomeScreen to utilize new components and styles.

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,16 @@
-import { Text, View } from "react-native-reanimated/lib/typescript/Animated";
+import { Box } from "@/components/ui/box";
+import { Text, View } from "react-native";
 
 export default function HomeScreen() {
 	return (
-		<View className="flex-1 items-center justify-center bg-white">
-			<Text className="text-xl font-bold text-blue-500">
+		<View className="flex-1 items-center justify-center">
+			<Text className="text-3xl font-bold text-blue-500">
 				Welcome to Nativewind!
 			</Text>
+
+			<Box>
+				<Text>Hello</Text>
+			</Box>
 		</View>
 	);
 }
