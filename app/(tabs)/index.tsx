e1f64dcfce4e39ff16e436545126b8a83bae6b96
raphaeltkorp <raphael@tkorp.com>
2025-10-27 22:34:37 +0100

    feat(home): update welcome message to include user's first name and add match finding button

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,18 @@
+import { useAuthSession } from "@/hooks/use-auth-session";
 import { Trans } from "@lingui/react/macro";
-import { Text, View } from "react-native";
+import { Text, TouchableOpacity, View } from "react-native";
 
 export default function HomeScreen() {
+	const { data: session } = useAuthSession();
+
 	return (
 		<View className="flex-1 items-center justify-center">
 			<Text className="text-3xl font-bold text-blue-500">
-				<Trans id="home-screen.title">Welcome!</Trans>
+				<Trans id="home-screen.title">Bienvenue {session?.user?.firstName}!</Trans>
 			</Text>
+			<TouchableOpacity className="mt-4 px-4 py-2 bg-blue-500 rounded">
+				<Text className="text-white text-xl">Trouver un match</Text>
+			</TouchableOpacity>
 		</View>
 	);
 }
