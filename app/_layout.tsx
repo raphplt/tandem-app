bef95cec559a4492e1df245d25e806845c1950fe
raphaeltkorp <raphael@tkorp.com>
2025-11-02 12:33:30 +0100

    Suppression route non utilisée

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -44,14 +44,7 @@ export default function RootLayout() {
 							<Stack>
 								<Stack.Screen name="(auth)" options={{ headerShown: false }} />
 								<Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-								<Stack.Screen
-									name="(onboarding)"
-									options={{ headerShown: false }}
-								/>
-								<Stack.Screen
-									name="modal"
-									options={{ presentation: "modal", title: "Modal" }}
-								/>
+								<Stack.Screen name="(onboarding)" options={{ headerShown: false }} />
 							</Stack>
 						</AuthProvider>
 					</QueryProvider>
